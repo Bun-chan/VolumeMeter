bad08d693fb9ef898b985b870a3c16cbb81fe6a9
bun chan <stephenjamesfitzgerald@yahoo.com>
2021-04-06 19:32:00 +0900

    created the project in Arduino IDE

---
+++ VolMetPrj/VolMetPrj.ino
@@ -0,0 +1,16 @@
+/*
+Volume Meter 2021-04-06
+LED brightness is controlled by volume.
+Amplifier uses 2N3904 transistor to amplify the electret mic output.
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
